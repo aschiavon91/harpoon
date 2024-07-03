5535cdc6169f5129ecc7bac6344771e2aa7b3776
Antonio <aschiavon91@gmail.com>
2024-07-02 23:24:24 -0300

    chore: bump app version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Harpoon.MixProject do
   def project do
     [
       app: :harpoon,
-      version: "1.7.0",
+      version: "1.8.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
