f5214f4719b43cebd872fb506cf2e441e54419de
Antonio <aschiavon91@gmail.com>
2025-01-18 14:59:01 -0300

    style: code format

--- config/runtime.exs
+++ config/runtime.exs
@@ -3,13 +3,8 @@ import Config
 if config_env() == :prod do
   user_home = System.fetch_env!("HOME")
 
-  config :harpoon, Harpoon.Repo,
-    database: System.get_env("DATABASE_FILE", "#{user_home}/.harpoon/harpoon_prod.sqlite"),
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
   generated_secret_key_base = 64 |> :crypto.strong_rand_bytes() |> Base.encode64(padding: false) |> binary_part(0, 64)
   secret_key_base = System.get_env("SECRET_KEY_BASE", generated_secret_key_base)
-
   host = System.get_env("PHX_HOST", "localhost")
   port = String.to_integer(System.get_env("PORT", "4000"))
   scheme = System.get_env("PHX_HOST_SCHEME", "http")
@@ -20,6 +15,14 @@ if config_env() == :prod do
     |> String.replace(~s/\"/, ~s//)
     |> String.split(",")
 
+  config :cors_plug,
+    origin: valid_origins,
+    max_age: 86_400
+
+  config :harpoon, Harpoon.Repo,
+    database: System.get_env("DATABASE_FILE", "#{user_home}/.harpoon/harpoon_prod.sqlite"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
   config :harpoon, HarpoonWeb.Endpoint,
     url: [host: host, port: port, scheme: scheme],
     server: true,
@@ -29,8 +32,4 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
-
-  config :cors_plug,
-    origin: valid_origins,
-    max_age: 86_400
 end
